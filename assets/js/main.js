61b1ad859fd0ed0ada5d60d9ffdd47250141ddbe
Reon Fernandes <reondias@gmail.com>
2024-04-20 12:59:34 +0530

    images-icons-style_code
    this folder contains styling stuff

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    const navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+}
